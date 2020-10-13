8b9897e7fefd9033433ade354b78b4e2eeb9706b
Elwood He <elwoodhe@ElwooddeMacBook-Pro.local>
2020-10-13 11:24:21 -0400

    Week 6

---
+++ activities/week6/week6.R
@@ -0,0 +1,73 @@
+## Week 6 Activity
+##
+## This script 
+##
+## Author: Group 5: Zhilin He, Yan Chen, Tianshi Wang, Hao He
+## Updated: October 13, 2020
+# 79: -------------------------------------------------------------------------
+
+## Read data
+
+library(tidyverse)
+
+pwc = function(n) {
+  # Format a function
+  # Inputs: n, an integer; Outputs: a formatted integer string
+  format(N, trim = FALSE, big.mark = ',')
+}
+
+url1 = paste("https://github.com/jbhender/Stats506_F20/raw/master/",
+             "problem_sets/data/nhanes_demo.csv", sep = "")
+url2 = paste("https://github.com/jbhender/Stats506_F20/raw/master/",
+             "problem_sets/data/nhanes_ohxden.csv", sep = "")
+
+if(!file.exists("nhanes_demo.csv")) {
+  download.file(url1, "nhanes_demo.csv")
+}
+if(!file.exists("nhanes_ohxden")) {
+  download.file(url2, "nhanes_ohxden.csv")
+}
+demo = read.csv("nhanes_demo.csv")
+ohxden = read.csv("nhanes_ohxden.csv")
+
+## Merge the data
+
+ohxden_1 = ohxden %>% select(SEQN, OHDDESTS) 
+merged = left_join(demo, ohxden_1, by = "SEQN")
+
+## Create a clean dataset
+
+clean = merged %>% 
+  mutate(gender = factor(RIAGENDR), 
+         under_20 = ifelse(RIDAGEYR<20, "Y", "N"),
+         college = ifelse(under_20 == "Y", "No college",
+                          ifelse(DMDEDUC2>=4, "College", "No college"))
+         ) %>% select(id = SEQN, age = RIDAGEYR, exam_status = RIDSTATR,
+                      ohx_status = OHDDESTS, gender, under_20, college)
+
+## Add a variable
+  
+clean = clean %>% mutate(ohx = ifelse(!is.na(ohx_status),
+                               ifelse(exam_status == 2 & ohx_status == 1, 
+                               "complete", "missing"), "missing"))
+
+## Filter the data
+
+df = clean %>% filter(exam_status == 2)
+
+## Construct a table
+
+table1 = df %>% group_by(under_20, ohx) %>% summarise(n=n()) %>%
+  pivot_wider(names_from = ohx, values_from = n)
+
+table2 = df %>% group_by(gender, ohx) %>% summarise(n=n()) %>%
+  pivot_wider(names_from = ohx, values_from = n)
+
+table3 = df %>% group_by(college, ohx) %>% summarise(n=n()) %>%
+  pivot_wider(names_from = ohx, values_from = n)
+
+table = rbind(table1, table2, table3)
+
+table[,1] = c("Not under 20", "under")
+
+## 
